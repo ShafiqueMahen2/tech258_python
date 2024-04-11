f78e970c69e66595c59a51b7be6d5887fbac3501
Shafique M <shafique_m@hotmail.co.uk>
2024-04-11 16:04:30 +0100

    Fifth commit - completed task 4

--- control_flow/while_loops.py
+++ control_flow/while_loops.py
@@ -1,3 +1,4 @@
+# Task 3
 x = 0
 
 while x < 10:
@@ -7,3 +8,15 @@ while x < 10:
     x+=1
 
 # If we didn't increment x each iteration, the value of x would remain the same, creating a infinite loop.
+
+# Task 4
+user_prompt = True
+
+while user_prompt:
+    age = input("What is your age?")
+    if age.isdigit() and int(age) <= 117:
+        user_prompt = False
+    else:
+        print("Inputted value is invalid or too high, please try again!")
+
+print(f"Your age is {age}")
